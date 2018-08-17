14b8e00041a55f4375d6959d4fc13195025d99f4
v_liuchaofan <v_liuchaofan@baidu.com>
2018-08-17 17:30:54 +0800

    小程序

---
+++ pages/home/home.js
@@ -0,0 +1,10 @@
+
+Page({
+    goLocation: function(){
+         swan.navigateTo({url: "/pages/location/location"})
+    },
+    goList: function () {
+        console.log(11)
+        /*swan.navigateTo({url: "/pages/location/location"})*/
+    }
+})
