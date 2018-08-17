719c4fee652d889ede79646e6662f38089c1d3b5
LiuChaofanw <353985744@qq.com>
2018-08-17 18:47:35 +0800

    list

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,6 @@ Page({
          swan.navigateTo({url: "/pages/location/location"})
     },
     goList: function () {
-        console.log(11)
-        /*swan.navigateTo({url: "/pages/location/location"})*/
+        swan.navigateTo({url: "/pages/list/list"})
     }
 })
